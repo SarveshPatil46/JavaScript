59ed4be56797df8c4ddbb578bd55dd2246686eaf
Sarvesh Patil <sarvesh2902@gmail.com>
2022-02-09 18:49:51 +0530

    Updated Linked list with insert and remove methods

--- ImplementLinkedList/ImplementLinkedList.js
+++ ImplementLinkedList/ImplementLinkedList.js
@@ -1,10 +1,3 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
 class LinkedList {
     constructor(value) {
         this.head = {
@@ -14,25 +7,73 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
-
     append(value) {
-        const newNode = new Node(value);
+        const newNode = {
+            value: value,
+            next: null
+        }
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
+        return this;
     }
-
     prepend(value) {
-        const newNode = new Node(value);
+        const newNode = {
+            value: value,
+            next: null
+        }
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
+        return this;
+    }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while(currentNode !== null){
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        console.log(array);
+    }
+    insert(index, value){
+        //Check for proper parameters;
+        if(index >= this.length) {
+            return this.append(value);
+        }
+
+        const newNode = {
+            value: value,
+            next: null
+        }
+        const leader = this.traverseToIndex(index-1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+    remove(index) {
+        const leader = this.traverseToIndex(index-1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
     }
 }
 
-const myLinkedList = new LinkedList(10);
+let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(2);
+myLinkedList.printList();
