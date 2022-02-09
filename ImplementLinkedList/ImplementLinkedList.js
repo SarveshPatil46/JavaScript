42707f44415c45eba7b92ad9fa3cee9de83570b1
Sarvesh Patil <sarvesh2902@gmail.com>
2022-02-09 18:50:55 +0530

    Updated Linked list with insert and remove methods

--- ImplementLinkedList/ImplementLinkedList.js
+++ ImplementLinkedList/ImplementLinkedList.js
@@ -7,6 +7,7 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
     append(value) {
         const newNode = {
             value: value,
@@ -17,6 +18,7 @@ class LinkedList {
         this.length++;
         return this;
     }
+
     prepend(value) {
         const newNode = {
             value: value,
@@ -27,18 +29,20 @@ class LinkedList {
         this.length++;
         return this;
     }
+
     printList() {
         const array = [];
         let currentNode = this.head;
-        while(currentNode !== null){
+        while (currentNode !== null) {
             array.push(currentNode.value)
             currentNode = currentNode.next
         }
         console.log(array);
     }
-    insert(index, value){
+
+    insert(index, value) {
         //Check for proper parameters;
-        if(index >= this.length) {
+        if (index >= this.length) {
             return this.append(value);
         }
 
@@ -46,23 +50,25 @@ class LinkedList {
             value: value,
             next: null
         }
-        const leader = this.traverseToIndex(index-1);
+        const leader = this.traverseToIndex(index - 1);
         const holdingPointer = leader.next;
         leader.next = newNode;
         newNode.next = holdingPointer;
         this.length++;
     }
+
     traverseToIndex(index) {
         let counter = 0;
         let currentNode = this.head;
-        while(counter !== index){
+        while (counter !== index) {
             currentNode = currentNode.next;
             counter++;
         }
         return currentNode;
     }
+
     remove(index) {
-        const leader = this.traverseToIndex(index-1);
+        const leader = this.traverseToIndex(index - 1);
         const unwantedNode = leader.next;
         leader.next = unwantedNode.next;
         this.length--;
